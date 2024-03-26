13d6036d85039ac5c2d5b52d055f986aefd817ad
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-26 22:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//traverse up the DOM tree
+function getDomLevel(elementId){
+    const targetElement=document.getElementById(elementId)
+
+    let domLevel=1;
+    let parentElement=targetElement.parentElement;
+    while(parentElement!==null && parentElement.tagName!=='HTML'){
+        domLevel++
+        parentElement=parentElement.parentElement
+}
+return domLevel+1
+}
+alert(getDomLevel('level'))
